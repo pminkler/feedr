68090db5ea36cc9a7efc5ff5e657cf9561592300
Phil Minkler <philipminkler@sussex.team>
2025-02-06 13:46:45 -0600

    "Update web app icons in manifest and add new favicon.svg"

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -81,12 +81,12 @@ export default defineNuxtConfig({
       background_color: "#ffffff",
       icons: [
         {
-          src: "/android-chrome-192x192.png",
+          src: "/web-app-manifest-192x192.png",
           sizes: "192x192",
           type: "image/png",
         },
         {
-          src: "/android-chrome-512x512.png",
+          src: "/web-app-manifest-512x512.png",
           sizes: "512x512",
           type: "image/png",
         },
