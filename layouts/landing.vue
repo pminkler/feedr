c1c5508a5a7884ed3cc94d151b0faaf8582446aa
Phil Minkler <philipminkler@sussex.team>
2025-03-14 16:21:35 -0500

    Add landing page layout with header, footer, and main content structure

---
+++ layouts/landing.vue
@@ -0,0 +1,16 @@
+<template>
+  <Header />
+
+  <UMain>
+    <UContainer>
+      <UPage>
+        <UPageBody>
+          <slot />
+        </UPageBody>
+      </UPage>
+    </UContainer>
+  </UMain>
+  <AppFooter />
+</template>
+
+<script lang="ts" setup></script>
